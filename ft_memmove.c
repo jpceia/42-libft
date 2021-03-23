645bcd247cfa5c0516303ab7b7cafcd57792b0a1
Joao pedro Ceia <jceia@c1r18s6.42lisboa.com>
2021-03-23 19:05:56 +0000

    changed ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 15:09:24 by jpceia            #+#    #+#             */
-/*   Updated: 2021/03/23 18:40:58 by jceia            ###   ########.fr       */
+/*   Updated: 2021/03/23 19:02:46 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,21 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	ptr_dest = (char *)dest;
 	ptr_src = (char *)src;
 	index = 0;
-	while (index < n)
+	if (src > dest)
 	{
-		ptr_dest[index] = ptr_src[index];
-		index++;
+		while (index < n)
+		{
+			ptr_dest[index] = ptr_src[index];
+			index++;
+		}
+	}
+	else
+	{
+		while (index < n)
+		{
+			ptr_dest[n - index - 1] = ptr_src[n - index - 1];
+			index++;
+		}
 	}
 	return (dest);
 }
